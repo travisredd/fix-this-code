03d2b7baebe37dd9e011b5ce7ae1483dcf03c299
travismredd <travismredd@mail.weber.edu>
2019-01-12 22:03:11 -0700

    Fixed the code

--- problem.js
+++ problem.js
@@ -5,16 +5,21 @@ function fizzBuzz(){
     //If divisible by 3 and 5, print fizzbuzz
     //Else, print the number
     for(i=1;i<=100;i++){
-        if(i % 3 === 0){
+        if(i % 3 === 0 && i % 5 === 0)
+		{
+			console.log("fizzbuzz");
+		}
+        else if(i % 3 === 0)
+        {
             console.log("fizz");
         }
-        else if(i % 5 === 0){
+        else if(i % 5 === 0)
+        {
             console.log("buzz");
         }
-        else{
+        else
+        {
             console.log(i);
         }
     }
 }
-
-fizzBuzz();
